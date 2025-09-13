ad219605f45c4fa00404267f63983db0524c7577
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-13 12:02:46 +0500

    canges

--- lect_4/src/App.jsx
+++ lect_4/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   
 
   return (
-   <h1 className="text-3xl font bg-red-400">my first project</h1>
+   <h1 className="text-3xl font-bold text-red-600">my first project</h1>
   )
 }
 
